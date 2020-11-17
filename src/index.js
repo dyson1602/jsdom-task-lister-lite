e5e6ffd375459e36626808eabefad59922c5c0b3
dyson1602 <cclark000@citymail.cuny.edu>
2020-11-17 18:22:03 -0500

    it works, but is ugly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  //*********DOM ELEMENTS**********//
+  const taskForm = document.querySelector("#create-task-form")
+  const newTaskUl = document.querySelector("#tasks")
+  const newTaskDesc = document.querySelector("#new-task-description")
+
+  console.log(taskForm)
+  //*********EVENT LISTENERS**********//
+
+  taskForm.addEventListener("submit", createNewTask)
+
+
+  //*********EVENT HANDLERS**********//
+
+  function createNewTask(event) {
+    event.preventDefault()
+    const task = document.createElement('li')
+    task.textContent = event.target.querySelector('[name="new-task-description"]').value
+    newTaskUl.append(task)
+
+    event.target.reset()
+  }
+
+
+
+  //*********RENDER FUNCTIONS**********//
+
+
+
+
+  //*********INITIALIZE***********//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
